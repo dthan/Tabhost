cd962d24a8284d7d39e1d0aad469e86e027ab59b
M Deden Firdaus <deden@uinsgd.ac.id>
2016-10-25 09:52:27 +0700

    PERUBAHAN 1

---
+++ app/src/main/java/com/labkomuin/hello/tabhost/Tab_1.java
@@ -0,0 +1,14 @@
+package com.labkomuin.hello.tabhost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tab_1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_1);
+        //tes isi kode program disini
+    }
+}
---
+++ app/src/main/java/com/labkomuin/hello/tabhost/Tab_3.java
@@ -0,0 +1,13 @@
+package com.labkomuin.hello.tabhost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tab_3 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_3);
+    }
+}
